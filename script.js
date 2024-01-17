88a82fde4c4d189692101284074eb1b128a18152
rupigithub <117827907+rupigithub@users.noreply.github.com>
2024-01-17 13:14:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	arr.sort();
+  let min_cost = 0;
+	let ref_cost = 0;
+	arr.foreach(v => {
+		ref_cost += v;
+		min_cost += ref_cost;
+	})
+	return min_cost;
 }
 
 module.exports=mincost;
